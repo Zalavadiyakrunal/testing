c50397db919cf4fb41779f36de17b5b1734608c9
Krunal1109 <krunal.krz@gmail.com>
2023-02-03 11:07:54 +0000

    this for add

--- src/testing/Tasting.java
+++ src/testing/Tasting.java
@@ -3,7 +3,11 @@ package testing;
 public class Tasting {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	double a =1.98;
+	double x= 3.09;
+	double w= a+x;
+	System.out.println("wis = "+ w);
+	
 
 	}
 
